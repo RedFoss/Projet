00a6dcbf2b3d0812e535132efb07dc956162d471
Quentin Gimié D'Arnaud <quentin.gimiedarnaud@gmail.com>
2018-05-23 10:39:28 +0200

    Add files via upload

---
+++ Angularjs/controller/controller.js
@@ -0,0 +1,19 @@
+var app = angular.module("app",[]);
+
+app.controller("headerCtrl",function($scope)){
+	//....
+});
+
+app.controller("footerCtrl",function($scope)){
+	//...
+});
+app.controller("menuCtr",function($scope)){
+	//..
+});
+
+app.controller("contentCtr",function($scope)){
+	//..
+}
+app.controller("contentCtr",function($scope)){
+	//..
+});
